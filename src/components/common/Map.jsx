e709b0fa987cf69bac04a91bbd12d93364c2786c
tubi55 <tubi55@nate.com>
2024-10-11 15:24:42 +0900

    토글 버튼 그룹 스타일링

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -35,7 +35,6 @@ export default function Map() {
 		image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos)
 	});
 
-	//타입컨트롤, 줌컨트롤 인스턴스 생성
 	const instType = new kakao.maps.MapTypeControl();
 	const instZoom = new kakao.maps.ZoomControl();
 
@@ -48,13 +47,8 @@ export default function Map() {
 		ref_mapFrame.current.innerHTML = '';
 		ref_instMap.current = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
 		inst_marker.setMap(ref_instMap.current);
-
 		[instType, instZoom].forEach(inst => ref_instMap.current.addControl(inst));
 
-		//해당 전역 이벤트연결 구문이 빈 의존성 배열의 콜백안쪽에 등록되었을때의 문제점
-		//지점 버튼을 클릭하여 Index 상태값이 변경되면 리사이즈시 갱신된 화면으로 이벤트 연결 불가
-		//window에 연결되는 initPos핸들러 함수는 내부적으로 지도인스턴에 위치인스턴스값을 활용해서 위치를 갱신하는 구조
-		//리사이즈될떄마다 인스턴스정보값이 갱신되어야 하므로 Index 의존성 배열 안쪽의 콜백에서 호출
 		window.addEventListener('resize', initPos);
 		return () => window.removeEventListener('resize', initPos);
 	}, [Index]);
@@ -72,6 +66,11 @@ export default function Map() {
 							{el.title}
 						</li>
 					))}
+				</ul>
+
+				<ul className='btnToggleSet'>
+					<li>Traffic</li>
+					<li>Roadview</li>
 				</ul>
 			</nav>
 		</section>
