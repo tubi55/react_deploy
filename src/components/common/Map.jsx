4fcbf25b2107e37bfc55fb0425ee3295403cd287
tubi55 <tubi55@nate.com>
2024-10-11 09:31:58 +0900

    기본 지도 인스턴스 리액트 구조에 맞게 생성

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef } from 'react';
+
 export default function Map() {
-	console.log(kakao);
+	const { kakao } = window;
+	const ref_mapFrame = useRef(null);
+
+	const mapOption = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667)
+	};
+
+	useEffect(() => {
+		new kakao.maps.Map(ref_mapFrame.current, mapOption);
+	}, []);
+
 	return (
 		<section className='map'>
 			<h2>Location</h2>
 
-			<figure className='mapFrame'></figure>
+			<figure ref={ref_mapFrame} className='mapFrame'></figure>
 		</section>
 	);
 }
