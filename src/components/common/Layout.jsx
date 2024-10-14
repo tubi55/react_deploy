00c9918f87e2306fd1e78c4ec23e6fcef8b8850d
tubi55 <tubi55@nate.com>
2024-10-14 11:46:35 +0900

    라우터로 페이지 컴포넌트 이동시 스크롤 위치값 상단으로 자동 초기화

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,10 +1,16 @@
 import { useLocation } from 'react-router-dom';
 import Mask from './Mask';
 import SplitText from './SplitText';
+import { useEffect } from 'react';
 
 export default function Layout({ title, children }) {
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
+
+	//라우터를 통해 새로운 페이지 컴포넌트 마운트시 강제로 윈도우의 스크롤 위치값을 0으로 초기화
+	useEffect(() => {
+		window.scrollTo({ top: 0 });
+	}, []);
 
 	return (
 		<>
