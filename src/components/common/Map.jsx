f3ab303b053f7898b4137e5e745e9e873e1e25e4
tubi55 <tubi55@nate.com>
2024-10-14 10:08:56 +0900

    두번째 useEffect문에 의존성 배열 추가

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -54,15 +54,6 @@ export default function Map() {
 		ref_instClient.current.getNearestPanoId(latlng, 50, panoId => ref_instView.current.setPanoId(panoId, latlng));
 	}, [kakao, latlng, markerImg, markerSize, markerPos]);
 
-	//initPos, createMap을 useEffect외부로 분리하면 의존성배열에 등록하라는 권고메세지 뜨는 이유
-	//이유: 해당 외부함수는 상태값을 활용해서 동작되는 함수, 해당 함수가 외부에서 변경될수도 있다고 인지하기 때문에 해당 함수도 의존성 배열에 등록 요청
-
-	//initPos, createMap을 useEffect의 의존성 배열에 등록시 다시 해당 함수자체에 useCallback처리하라는 권고문구 뜨는 이유
-	//이유: 여러 상태값이 useEffect안에 의존성배열 형태로 같이 등록되어 있기 때문에 함수자체를 메모이제이션해서 기존 함수 내용을 재사용하기 위함
-
-	//해당 함수를 useCallback으로 메모이제이션 처리하면 다시 의존성 배열에 권고사항이 뜨는 이유
-	//이유: 해당 메모이제이션 처리하긴 했는데 메모이제이션한 함수 내부에 상태값을 의존하는 구문이 포함되어 있으면 해당 상태값 변경시 함수를 재연산해야 되기 때문
-
 	useEffect(() => {
 		createMap();
 
@@ -72,7 +63,7 @@ export default function Map() {
 
 	useEffect(() => {
 		Traffic ? ref_instMap.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : ref_instMap.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-	}, [Traffic]);
+	}, [Traffic, kakao]);
 
 	return (
 		<section className='map'>
