74ac7e9347c88edcfa9adcf97073658e860484ef
tubi55 <tubi55@nate.com>
2024-10-14 16:15:34 +0900

    커스텀훅 호출시 전달된 옵션값을 쿼리키 배열에 등록하여 고유쿼리키 설정

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -10,11 +10,7 @@ export default function Youtube() {
 	const shortenText = useShortenText();
 	const combineText = useCombineText();
 
-	//useQuery기능이 내장된 유튜브데이터 가져오는 커스텀훅 호출
-	//useQuery가 반환하는 결과값중 자주쓰는 프로퍼티 정리
-	//data:실제반환받은 서버데이터 / isPending: 요청대기 유무 / isError: 데이터반환 실패 유무 / error: 데이터요청 실패시 반환받는 에러 정보객체
-	//아래와같이 리액트쿼리를 활용한 커스텀훅으로 서버데이터를 받으면 기존처럼 지저분하게 useState, useEffect, useCallback훅을 사용할 필요가 없음
-	const { data: Vids, isPending } = useYoutubeQuery();
+	const { data: Vids, isPending } = useYoutubeQuery({ type: 'B' });
 
 	return (
 		<Layout title={'YOUTUBE'}>
--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -1,11 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 
 //아래 커스텀훅에서 활용될 fetching 함수
-const fetchYoutube = async () => {
+const fetchYoutube = async ({ queryKey }) => {
+	//useQuery로 전달된 opt객체는 fetching함수 안쪽의 queryKey로 전달받음
+	console.log(queryKey[1]); //{type:'A'}
 	const api_key = import.meta.env.VITE_YOUTUBE_API;
-	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+	const pidA = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const pidB = 'PLYOPkdUKSFgUJeKpDIbI0H1wwgQO-hCZF';
 	const num = 10;
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
+	let url = '';
+	const urlA = `${baseURL}?part=snippet&playlistId=${pidA}&key=${api_key}&maxResults=${num}`;
+	const urlB = `${baseURL}?part=snippet&playlistId=${pidB}&key=${api_key}&maxResults=${num}`;
+
+	//전달된 queryKey 옵션값에 따라서 최종 요청 url변형후 fetch요청
+	queryKey[1].type === 'A' && (url = urlA);
+	queryKey[1].type === 'B' && (url = urlB);
 
 	const data = await fetch(url);
 	const json = await data.json();
@@ -13,11 +23,12 @@ const fetchYoutube = async () => {
 };
 
 //useQuery기능이 내장된 실제 호출될 커스텀훅
-export const useYoutubeQuery = () => {
+export const useYoutubeQuery = (opt = { type: 'A' }) => {
 	return useQuery({
-		queryKey: ['youtubeList'],
+		//결과적으로 커스텀훅 호출시 옵션값을 쿼리키 배열의 두번쨰요소에 등록하면, 해당 옵션값에 따른 고유쿼리키가 자동 생성
+		queryKey: ['youtubeList', opt],
 		queryFn: fetchYoutube,
-		staleTime: 1000 * 10,
+		staleTime: 1000 * 60,
 		gcTime: 1000 * 60
 	});
 };
