7b7f35d15639c4f4a6f1d708f19492a9078bc5cd
tubi55 <tubi55@nate.com>
2024-10-14 14:35:32 +0900

    기존 fetch then구문을 async await문으로 코드 가독성 개선

--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -1,5 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
 
+const fetchYoutube = async () => {
+	const api_key = import.meta.env.VITE_YOUTUBE_API;
+	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
+
+	//promise를 반환하는 함수 앞쪽에 await 등록하면 해당함수가 아무리 시간오래걸려도 결과값와 완료될때까지 다음구문이
+	const data = await fetch(url);
+	const json = await data.json();
+	return json.items;
+
+	// fetch(url)
+	// 	.then(data => data.json())
+	// 	.then(json => {
+	// 		return json.items;
+	// 	});
+};
+
 /*
   1.유튜브 데이터 fetching함수 (전달될 인수값 필요)
 
