2a86fca6acd45b9c8e7a1ef6dc6fa2b1e940aadf
tubi55 <tubi55@nate.com>
2024-10-14 09:17:17 +0900

    memo, useMemo, useCallback의 개념 설명

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -3,12 +3,10 @@ import { useEffect, useRef, useState } from 'react';
 export default function Map() {
 	const { kakao } = window;
 
-	//화면 렌더링에 필요한 state값 초기화
 	const [Index, setIndex] = useState(0);
 	const [Traffic, setTraffic] = useState(false);
 	const [Roadview, setRoadview] = useState(false);
 
-	//지점정보가 담긴 참조객체를 생성하고 현재 활성화된 Index순번의 데이터 추출
 	const ref_info = useRef([
 		{
 			title: 'COEX',
@@ -33,47 +31,32 @@ export default function Map() {
 		}
 	]);
 	const { latlng, markerImg, markerSize, markerPos } = ref_info.current[Index];
-
-	//순수함수 형태로 값을 바로 전달받을 수 있는 인스턴스값 참조객체에 담음
 	const ref_instClient = useRef(new kakao.maps.RoadviewClient());
 	const ref_instType = useRef(new kakao.maps.MapTypeControl());
 	const ref_instZoom = useRef(new kakao.maps.ZoomControl());
-
-	//컴포넌트 마운트시에만 전달받을 수 있는 빈 참조객체 생성
 	const ref_mapFrame = useRef(null);
 	const ref_viewFrame = useRef(null);
 	const ref_instMap = useRef(null);
 	const ref_instMarker = useRef(null);
 	const ref_instView = useRef(null);
 
-	//리사이즈 이벤트에 연결될 화면위치 초기화 함수
 	const initPos = () => ref_instMap.current.setCenter(latlng);
 
-	//Index값이 변경될때마다 지도초기화, 뷰,마커, 로드뷰인스턴스 생성 및 리사이즈 이벤트연결
 	useEffect(() => {
 		ref_mapFrame.current.innerHTML = '';
-
-		//맵, 마커, 로드뷰, 로드뷰 인스턴스 생성후 미리 생성한 참조객체에 옮겨담음
 		ref_instMap.current = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
 		ref_instMarker.current = new kakao.maps.Marker({ position: latlng, image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos) });
 		ref_instView.current = new kakao.maps.Roadview(ref_viewFrame.current);
-
-		//마커 인스턴스에 지도 인스턴스 바인딩해서 지도에 마커 출력
 		ref_instMarker.current.setMap(ref_instMap.current);
 
-		//교통정보, 로드뷰 토글 상태 변경함수 반복호출해서 초기화 및 컨트롤러 반복호출하여 초기화
 		[setTraffic, setRoadview].forEach(func => func(false));
 		[ref_instType.current, ref_instZoom.current].forEach(inst => ref_instMap.current.addControl(inst));
-
-		//로드뷰 인스턴스에 panoId연결해 실제 로드뷰화면 출력하는 호출문
 		ref_instClient.current.getNearestPanoId(latlng, 50, panoId => ref_instView.current.setPanoId(panoId, latlng));
 
-		//윈도우 전역 객체에 resize 이벤트 핸들러 연결 및 제거
 		window.addEventListener('resize', initPos);
 		return () => window.removeEventListener('resize', initPos);
 	}, [Index]);
 
-	//Traffic 값이 반전될때마다 트레픽 레이어 토글
 	useEffect(() => {
 		Traffic ? ref_instMap.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : ref_instMap.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
@@ -111,4 +94,18 @@ export default function Map() {
 	);
 }
 
-//미션 - 5시 40분까지 위의 분류된 카테고리 항목별로 코드 기능 분석 및 정리
+/*
+	리액트에서 함수형 컴포넌트의 개념
+	- 리액트는 클로저환경의 함수형 프로그래밍 기반
+	- 함수형 프로그래밍 : 함수한에 지역변수를 만들고 함수안쪽에서 또다른 함수를 리턴해서 안쪽의 지역변수를 재사용가능하게 유지하는 프로그래밍적 방법
+	- 리액트에서는 함수가 jsx반환 (컴포넌트), 함수가 특정 또다른 기능연산을 위한 함수나, 연산값을 반환 (훅)
+	- 위와 같은 원리를 통해서 리액트에서 useRef를 통한 참조객체, state값이 해당 컴포넌트 함수를 재렌더링(재호출하더라도) 유지됨
+
+	리액트에서의 메모이제이션
+	- 컴포넌트안쪽에서 활용하지만 반환되지는 않는 특정 값, 함수를 강제로 메모리에 등록해서 재호출될때마다 재연산하지 않도록 처리 
+	- memo : 컴포넌트 자체를 메모이제이션
+	- useMemo : 복잡한 연산과정을 필요로하는 연산된 결과값 (특정 함수의 반환값) 자체를 메모이제이션, 컴포넌트 재랜더링시 같은 값일 경우 기존값 재사용
+	- useCallback 
+	: 함수자체를 메모이제이션, 컴포넌트에 인수로 함수 전달시 같은 내용이란것을 인지시켜 불필요한 재랜더링 방지, 
+	: 코드의 가독성을 위해서 useEffect안쪽의 사용할 내용들을 함수로 묶어서 외부에 관리할때, 해당 함수에 필요한 의존성 배열을 연결할때
+*/
