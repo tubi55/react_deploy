815d484a5bbcdebff54a1ba22e4319b1f3d765c3
tubi55 <tubi55@nate.com>
2024-10-15 09:21:05 +0900

    메인 페이지에서만 전용 클래스명 적용

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -6,6 +6,14 @@ import { useEffect } from 'react';
 export default function Layout({ title, children }) {
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
+	console.log(pathname);
+
+	let currentClass = '';
+
+	//path명을 통해 레이아웃에 다른 클래스명 적용
+	if (isDetail) currentClass = 'detail';
+	else if (pathname === '/') currentClass = 'main';
+	else currentClass = title.toLowerCase();
 
 	useEffect(() => {
 		window.scrollTo({ top: 0 });
@@ -13,7 +21,7 @@ export default function Layout({ title, children }) {
 
 	return (
 		<>
-			<main className={isDetail ? 'detail' : title.toLowerCase()}>
+			<main className={currentClass}>
 				<SplitText delay={0.5}>{title}</SplitText>
 				<section>{children}</section>
 			</main>
