6e07f1f6bf04f06e0a8b6c7ef94db25afc2c6b23
tubi55 <tubi55@nate.com>
2024-10-15 15:26:43 +0900

    기본 스와이퍼 옵션 적용

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -7,12 +7,12 @@ export default function Visual() {
 	const { data } = useFlickrQuery({ type: 'mine' });
 	return (
 		<figure className='visual'>
-			<Swiper>
+			<Swiper slidesPerView={3} spaceBetween={100} loop={true}>
 				{data?.map((pic, idx) => {
-					if (idx >= 3) return null;
+					if (idx >= 10) return null;
 					return (
 						<SwiperSlide key={idx}>
-							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg`} style={{ width: '100%', height: '100%' }} />
+							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow />
 						</SwiperSlide>
 					);
 				})}
