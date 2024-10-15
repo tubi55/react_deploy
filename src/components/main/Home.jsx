4e1e830d4cb112b62ad54859963d2a44db90b799
tubi55 <tubi55@nate.com>
2024-10-15 09:09:51 +0900

    visual 컴포넌트에 유튜브 미리보기 데이터 컨텐츠 분리

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,22 +1,10 @@
-import { useYoutubeQuery } from '../../hooks/useYoutube';
 import Layout from '../common/Layout';
-import Pic from '../common/Pic';
+import Visual from './Visual';
 
 export default function Home() {
-	const { data } = useYoutubeQuery();
-
 	return (
 		<Layout title={'HOME'}>
-			<div style={{ display: 'flex', gap: 40 }}>
-				{data?.map((vid, idx) => {
-					if (idx >= 3) return null;
-					return (
-						<article key={idx}>
-							<Pic src={vid.snippet.thumbnails.high.url} style={{ width: 400, height: 200 }} shadow />
-						</article>
-					);
-				})}
-			</div>
+			<Visual />
 		</Layout>
 	);
 }
