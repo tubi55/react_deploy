a7e4b53dbe5cd979162c5d0a0849adddf8c41f7d
tubi55 <tubi55@nate.com>
2024-10-15 16:35:31 +0900

    스타일 변경

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -2,20 +2,25 @@ import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { useState } from 'react';
 
 export default function Visual() {
+	const [Index, setIndex] = useState(0);
 	const { data } = useFlickrQuery({ type: 'mine' });
 	return (
 		<figure className='visual'>
 			{/* Img titles */}
 			<div className='textBox'>
 				{data?.map((el, idx) => (
-					<h2 key={idx}>{el.title.substr(0, 30)}</h2>
+					<h2 key={idx} className={Index === idx ? 'on' : ''}>
+						{el.title.substr(0, 30)}
+					</h2>
 				))}
 			</div>
 
 			{/* Img Pics */}
-			<Swiper slidesPerView={3} spaceBetween={100} loop={true} centeredSlides={true}>
+			{/* onSlideChange 이벤트 발생시 내부 순서값 구하는 프로퍼티로 index (loop:x), realIndex (loop: 0) */}
+			<Swiper slidesPerView={3} spaceBetween={100} loop={true} centeredSlides={true} onSlideChange={el => setIndex(el.realIndex)}>
 				{data?.map((pic, idx) => {
 					if (idx >= 10) return null;
 					return (
