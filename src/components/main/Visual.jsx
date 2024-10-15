415cb8078fbd4d99711f2e598e2ca9af6bf65ebd
tubi55 <tubi55@nate.com>
2024-10-15 15:18:49 +0900

    swiper 기본 스타일링

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -12,7 +12,7 @@ export default function Visual() {
 					if (idx >= 3) return null;
 					return (
 						<SwiperSlide key={idx}>
-							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg`} style={{ width: '100%', height: '100vh' }} />
+							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg`} style={{ width: '100%', height: '100%' }} />
 						</SwiperSlide>
 					);
 				})}
