d607770f1f4738a06e35e74716c2ef1e7c6178e3
tubi55 <tubi55@nate.com>
2024-10-15 17:44:31 +0900

    스와이퍼 오토플레이 오류 해결

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -1,15 +1,16 @@
 import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import { useEffect, useRef, useState } from 'react';
 import 'swiper/css';
-import { useState } from 'react';
 
 export default function Visual() {
 	const [Index, setIndex] = useState(0);
-	const { data } = useFlickrQuery({ type: 'mine' });
+	const { data, isSuccess } = useFlickrQuery({ type: 'mine' });
+
 	return (
 		<figure className='visual'>
-			{/* Img titles */}
 			<div className='textBox'>
 				{data?.map((el, idx) => (
 					<h2 key={idx} className={Index === idx ? 'on' : ''}>
@@ -18,20 +19,34 @@ export default function Visual() {
 				))}
 			</div>
 
-			{/* Img Pics */}
-			{/* onSlideChange 이벤트 발생시 내부 순서값 구하는 프로퍼티로 index (loop:x), realIndex (loop: 0) */}
-			<Swiper slidesPerView={3} spaceBetween={100} loop={true} centeredSlides={true} onSlideChange={el => setIndex(el.realIndex)}>
-				{data?.map((pic, idx) => {
-					if (idx >= 10) return null;
-					return (
-						<SwiperSlide key={idx}>
-							{/* swiperSlide요소에는 바로 css모션 스타일 적용 비권장 */}
-							<div className='inner'>
-								<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow />
-							</div>
-						</SwiperSlide>
-					);
-				})}
+			<Swiper
+				modules={[Autoplay]}
+				slidesPerView={3}
+				spaceBetween={100}
+				loop={true}
+				centeredSlides={true}
+				onSlideChange={el => setIndex(el.realIndex)}
+				autoplay={{
+					delay: 1000,
+					disableOnInteraction: true
+				}}
+				onSwiper={swiper => {
+					console.log(swiper);
+					setTimeout(() => {
+						swiper.autoplay.start();
+					}, 1000);
+				}}>
+				{isSuccess &&
+					data.map((pic, idx) => {
+						if (idx >= 10) return null;
+						return (
+							<SwiperSlide key={idx}>
+								<div className='inner'>
+									<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow />
+								</div>
+							</SwiperSlide>
+						);
+					})}
 			</Swiper>
 		</figure>
 	);
