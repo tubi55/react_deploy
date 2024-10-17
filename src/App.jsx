587915fe7a48c1a8e1c94420f5bd8af2343d8486
tubi55 <tubi55@nate.com>
2024-10-17 18:04:46 +0900

    모바일 패널 모션 사라지는 효과 유지 및 안쪽 윈도우 이벤트 핸들러 해제 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,9 @@ import MobileMenu from './components/common/MobileMenu';
 import { useGlobalState } from './hooks/useGlobal';
 
 export default function App() {
-	const { store } = useGlobalState();
 	const location = useLocation();
+	const { store } = useGlobalState();
+
 	return (
 		<>
 			<Header />
@@ -31,7 +32,8 @@ export default function App() {
 				</Routes>
 			</AnimatePresence>
 
-			{store.isMenu && <MobileMenu />}
+			{/* MobileMenu 컴포넌트 언마운트시 사라지는 모션이 끝날때까지 대기시키기 위해 AnimatePresence로 감싸줌 */}
+			<AnimatePresence>{store.isMenu && <MobileMenu />}</AnimatePresence>
 
 			<Footer />
 		</>
