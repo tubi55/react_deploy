91dd7c535c490e3d89fef83a3e2e76b733e15b8c
tubi55 <tubi55@nate.com>
2024-10-17 09:51:45 +0900

    GlobalData상태관리 컴포넌트 생성 및 활용 (오류버전)

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { GlobalProvider } from './hooks/useGlobalContext.js';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
 		<BrowserRouter>
-			<App />
+			<GlobalProvider>
+				<App />
+			</GlobalProvider>
 		</BrowserRouter>
 		<ReactQueryDevtools />
 	</QueryClientProvider>
