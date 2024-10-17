d02ca0d5befd2bb50e9d45150550790e0043f26f
tubi55 <tubi55@nate.com>
2024-10-17 20:48:36 +0900

    test

--- src/components/common/MobileMenu.jsx
+++ src/components/common/MobileMenu.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import useThrottle from '../../hooks/useThrottle';
 
 export default function MobileMenu() {
+	console.log('MobileMenu');
 	const { dispatch } = useGlobalState();
 
 	const { initial, animate, exit, transition } = {
