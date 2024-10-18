318303b91ff8ec8eecf67e205e74f9fab6812270
tubi55 <tubi55@nate.com>
2024-10-18 13:50:29 +0900

    Menu관련 전역 스테이트 및 스테이트 변경함수 연결

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,11 @@
 import { FaBars, FaEnvelope, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
-import { useGlobalDispatch, ACTIONS } from '../../hooks/useGlobal';
+import { useZunstandStore } from '../../hooks/useZustand';
 
 export default function Header() {
 	console.log('header');
-	const { dispatch } = useGlobalDispatch();
+	const { setMenuToggle } = useZunstandStore();
+
 	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
 	const snsArr = [FaEnvelope, FaInstagram, FaYoutube];
 	const { pathname } = useLocation();
@@ -36,7 +37,7 @@ export default function Header() {
 					</ul>
 				</nav>
 
-				<span className='btnMobile' onClick={() => dispatch({ type: ACTIONS.SET_MENU_TOGGLE })}>
+				<span className='btnMobile' onClick={setMenuToggle}>
 					<FaBars />
 				</span>
 			</header>
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { GlobalProvider } from './GlobalProvider';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
 		<BrowserRouter>
-			<GlobalProvider>
-				<App />
-			</GlobalProvider>
+			<App />
 		</BrowserRouter>
 		<ReactQueryDevtools />
 	</QueryClientProvider>
